d77359da7bd8645869e672ddd508a70f5bcc7e08
Jose Pablo Canales Maturana <jose-pablo1@hotmail.com>
2024-09-22 13:21:48 -0300

    FEAT: se agregaron los parametros GET y POST en app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ def index():
     return render_template("html/index.html")
 
 
-@app.route("/agregar-donacion")
+@app.route("/agregar-donacion" , methods=("GET", "POST"))
 def agregarDonacion():
     return render_template("html/agregar-donacion.html")
 
-@app.route("/informacion-dispositivo")
+@app.route("/informacion-dispositivo", methods=("GET"))
 def informacionDispositivo():
     return render_template("html/informacion-dispositivo.html")
 
-@app.route("/ver-dispositivos")
+@app.route("/ver-dispositivos", methods=("GET"))
 def verDispositivos():
     return render_template("html/ver-dispositivos.html")
