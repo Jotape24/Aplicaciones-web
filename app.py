b5f5e2e3d39c07a8f16ebc18214892bdc51dcf44
Jose Pablo Canales Maturana <jose-pablo1@hotmail.com>
2024-10-10 16:54:06 -0300

    feat: se agreagaron comunas y foto dispositivo

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 from utils.validacion import validarDonante, validarDispositivo, validarForm
+from database import db
+
 
 app = Flask(__name__)
 
